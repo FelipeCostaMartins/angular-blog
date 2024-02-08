f7bd240ffb8bd8e0ff70e467685310a342cc098b
Fê <felipecosta2407@gmail.com>
2024-02-08 15:10:03 -0300

    responsiveness and design completed
    Terminei as minhas alterações do projeto, deixando ele super responsivo e melhorando o design base.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,64 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+
+    "title": "SOBRE MIM: Engenheiro de Software em Potencial",
+
+    "description": `Olá, mundo! Eu sou aquele cara de 19 anos que ainda não sabe como passou tão rápido da infância para a vida adulta. Sou estudante de engenharia de software e, sim, ainda estou tentando descobrir a diferença entre um algoritmo e uma batata. Mas hey, estou aqui para aprender e me divertir enquanto faço isso!<br><br>
+
+    Desde que me entendo por gente, sempre fui apaixonado por tecnologia. Sério, eu acho que nasci com um mouse e um teclado na mão. Se não estou codificando, provavelmente estou jogando algum videogame ou assistindo a um anime. Ah, e se você me perguntar qual é o meu jutsu favorito de Naruto, prepare-se para uma conversa de horas.<br><br>
+
+    Falando em código, sou meio que um ninja do Front-end. HTML, CSS e JavaScript são como meus melhores amigos. E agora estou mergulhando de cabeça no maravilhoso mundo do Angular. Eu diria que estou dominando o Angular tão bem quanto dominava o controle remoto da TV aos 5 anos. (Ok, talvez não tão bem, mas estou chegando lá!)<br><br>
+
+    Mas sabe qual é a parte realmente interessante? Estou obcecado com blockchain. Sim, aquele negócio que parece mágica digital. Estou determinado a desvendar todos os seus segredos e me tornar um especialista em blockchain. Porque, vamos ser honestos, quem não quer ser o mago das criptomoedas?<br><br>
+
+    Então, se você quiser discutir sobre os últimos avanços na tecnologia ou apenas trocar piadas nerds, estou sempre pronto para a diversão!`,
+
+    "photoCover":"https://i.pinimg.com/originals/87/df/6d/87df6d60f4cc3c07968ae2127bddcc30.gif"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+
+    "title": "Meu portfólio",
+
+    "description": `Criação de uma carteira de criptomoedas utilizando javascript: <strong><a href="https://github.com/FelipeCostaMartins/BTC-wallet">BTC-wallet</a></strong> <br><br>
+
+    Projeto para explorar o efeito paralax apenas utilizando HTML e CSS: <strong><a href="https://github.com/FelipeCostaMartins/Projeto-cordel"> Projeto-cordel </a></strong> <br><br>
+
+    `,
+
+    "photoCover":"https://i.pinimg.com/originals/11/56/71/115671a1a70292994ba9ed0e000dfc27.gif"
+  },
+  {
+    "id":"3",
+
+    "title": "Revelando a Receita Secreta do Melhor Macarrão à Carbonara do Mundo",
+
+    "description": `Você já ouviu falar do Macarrão à Carbonara? Ah, meu amigo, prepare-se para uma experiência gastronômica de tirar o fôlego! Esta receita é tão secreta que até mesmo a Nonna ficaria de boca aberta. Mas, como somos amigos, vou compartilhar com você os ingredientes para o prato mais incrível que você já provou.<br><br>
+
+    Primeiro, pegue um punhado de macarrão al dente - afinal, ninguém gosta de uma massa mole como uma almofada de sofá! Em seguida, frite generosamente alguns pedaços de bacon até que estejam crocantes como chips. Ah, o aroma do bacon fritando... É como música para os nossos narizes!<br><br>
+
+    Enquanto isso, em uma tigela, bata alguns ovos frescos como se estivesse competindo na final de um reality show de culinária. Adicione uma generosa pitada de queijo pecorino e parmigiano - sim, estamos falando de queijo duplo aqui, porque mais queijo é sempre melhor!<br><br>
+
+    Agora vem o truque secreto: retire a panela do fogo e misture rapidamente o macarrão quente com o bacon. Adicione a mistura de ovos e queijo e mexa vigorosamente até que o calor da massa cozinhe os ovos e forme um molho sedoso e cremoso. Não esqueça de temperar com pimenta preta - ela é como o tempero da vida!<br><br>
+
+    E voilà! Você acabou de criar o melhor Macarrão à Carbonara do mundo. Sirva em pratos quentes, polvilhe com mais queijo (porque por que não?) e prepare-se para receber elogios como um verdadeiro chef italiano. Buon appetito!`,
+
+    "photoCover":"https://i.pinimg.com/originals/84/92/10/849210d020077dbce43eca13997fe667.gif"
+  },
+  {
+    "id":"4",
+
+    "title": "Angular é vida!",
+
+    "description": `Sabe por que o Angular não vai a festas?<br><br>
+
+    Porque ele sempre fica preso nas injeções de dependência!kkkkkkkk<br><br>
+
+    E por que o Angular é tão confiante?<br><br>
+
+    Porque ele sempre se sente em controle de todos os componentes da situação!kkkkkkkk`,
+
+    "photoCover":"https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbDVseDk3ZmIwNHJqaXI4eTAxc3pkamdrb2UwcGFwN25jYzE5cjM1MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3og0IJk9KmCUJASueQ/giphy.gif"
   }
 ]
